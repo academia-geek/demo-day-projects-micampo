d764eacac652799ce9e90cc27415925a7d95ea85
Diego Villalba <diego.villalba01@uptc.edu.co>
2022-07-12 16:41:44 -0500

    Rutas publicas y privadas

---
+++ src/containers/Logeado.jsx
@@ -0,0 +1,9 @@
+import React from 'react'
+
+const Logeado = () => {
+   return (
+      <div>Logeado</div>
+   )
+}
+
+export default Logeado
--- src/main.jsx
+++ src/main.jsx
@@ -1,8 +1,9 @@
 import React from 'react'
 import ReactDOM from 'react-dom/client'
+import AppRoutes from './routes/AppRoutes'
 
 ReactDOM.createRoot(document.getElementById('root')).render(
   <React.StrictMode>
-    <div>MiCampo</div>
+    <AppRoutes />
   </React.StrictMode>
 )
