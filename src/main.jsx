78cd7a4b8101b66c110494350d63bf18fa8062ab
Santiago <santiagovalencialeon@gmail.com>
2022-07-14 14:15:50 -0500

    google login realizado

--- src/main.jsx
+++ src/main.jsx
@@ -1,8 +1,14 @@
-import React from 'react'
-import ReactDOM from 'react-dom/client'
+import React from 'react';
+import ReactDOM from 'react-dom/client';
+import { Provider } from 'react-redux';
+import { store } from './app/store/store';
+import LogIn from './components/auth/LogIn';
+import Register from './components/auth/Register';
 
 ReactDOM.createRoot(document.getElementById('root')).render(
-  <React.StrictMode>
-    <div>MiCampo</div>
-  </React.StrictMode>
-)
+   <React.StrictMode>
+      <Provider store={store}>
+         <LogIn />
+      </Provider>
+   </React.StrictMode>
+);
