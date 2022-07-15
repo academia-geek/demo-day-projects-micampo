b54486dd07f0afef2012af0b54a0c5374ba9c068
Santiago <santiagovalencialeon@gmail.com>
2022-07-15 08:20:11 -0500

    login y registro listo

--- src/main.jsx
+++ src/main.jsx
@@ -9,7 +9,7 @@ ReactDOM.createRoot(document.getElementById('root')).render(
    <React.StrictMode>
       <Provider store={store}>
          {/* <LogIn /> */}
-         {/* <Register /> */}
+         <Register />
       </Provider>
    </React.StrictMode>
 );
