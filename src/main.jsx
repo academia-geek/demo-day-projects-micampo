bf5bbee7a059b1ebf64c19f7beb3caa7b9c9be3e
Santiago <santiagovalencialeon@gmail.com>
2022-07-18 03:53:51 -0500

    añadido base de datos de usuarios

--- src/main.jsx
+++ src/main.jsx
@@ -2,20 +2,13 @@ import React from 'react';
 import ReactDOM from 'react-dom/client';
 import { Provider } from 'react-redux';
 import { store } from './app/store/store';
-import LogIn from './components/auth/LogIn';
-import Register from './components/auth/Register';
-import ValidatePhone from './components/ValidatePhone';
-import './styles/main.css';
-import User from './containers/User';
 import 'animate.css';
+import AppRoutes from './routes/AppRoutes';
 
 ReactDOM.createRoot(document.getElementById('root')).render(
    <React.StrictMode>
       <Provider store={store}>
-         {/* <LogIn /> */}
-         {/* <Register /> */}
-         <User />
-         {/* <ValidatePhone /> */}
+         <AppRoutes />
       </Provider>
    </React.StrictMode>
 );
