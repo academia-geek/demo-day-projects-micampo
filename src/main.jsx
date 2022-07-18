560bfa50fd53d56966a8c1f6de804c62ec6e3d0f
Santiago <santiagovalencialeon@gmail.com>
2022-07-18 02:11:16 -0500

    añadido estilos y editar perfil

--- src/main.jsx
+++ src/main.jsx
@@ -4,12 +4,18 @@ import { Provider } from 'react-redux';
 import { store } from './app/store/store';
 import LogIn from './components/auth/LogIn';
 import Register from './components/auth/Register';
+import ValidatePhone from './components/ValidatePhone';
+import './styles/main.css';
+import User from './containers/User';
+import 'animate.css';
 
 ReactDOM.createRoot(document.getElementById('root')).render(
    <React.StrictMode>
       <Provider store={store}>
          {/* <LogIn /> */}
-         <Register />
+         {/* <Register /> */}
+         <User />
+         {/* <ValidatePhone /> */}
       </Provider>
    </React.StrictMode>
 );
