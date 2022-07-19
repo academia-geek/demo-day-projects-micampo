7724a2da8e2459ec5d7cf1e07ba6f3ead0c45f4f
Santiago <santiagovalencialeon@gmail.com>
2022-07-19 16:30:20 -0500

    mejoradas las rutas publicas y privadas

--- src/routes/AppRoutes.jsx
+++ src/routes/AppRoutes.jsx
@@ -1,9 +1,11 @@
-import React, { useEffect, useState } from 'react'
-import Home from "../containers/Home";
-import QuienesSomos from "../containers/QuienesSomos";
-import { BrowserRouter, Route, Routes } from 'react-router-dom'
+import React, { useEffect, useState } from 'react';
+import { BrowserRouter, Route, Routes } from 'react-router-dom';
+import { useSelector } from 'react-redux';
 import PublicRoutes from './PublicRoutes';
 import PrivateRoutes from './PrivateRoutes';
+
+import Home from '../containers/Home';
+import QuienesSomos from '../containers/QuienesSomos';
 import DashboardRoutes from './DashboardRoutes';
 import Nav1 from '../components/Navbar';
 import Aliados from '../containers/Aliados';
@@ -12,89 +14,132 @@ import Us from '../containers/Us';
 import Mercado from '../containers/Mercado';
 import LogIn from '../components/auth/Login';
 import Register from '../components/auth/Register';
+import User from '../containers/User';
+import LoadingScreen from '../components/LoadingScreen';
+import { getAuth, onAuthStateChanged } from 'firebase/auth';
 
 const AppRoutes = () => {
-   const [checkAuth, setCheckAut] = useState(true)
-   const [isLogget, setIsLogget] = useState(true)
+   const [checkAuth, setCheckAut] = useState(true);
+   const [isLogget, setIsLogget] = useState(false);
 
-   // useEffect(() => {
-   //    const auth = getAuth();
-   //    onAuthStateChanged(auth, (user) => {
-   //       if (user?.uid) {
-   //          //console.log(user);
-   //          setIsLogget(true)
-   //       } else {
-   //          setIsLogget(false)
-   //       }
-   //       setCheckAut(false)
-   //    })
-   // }, [setIsLogget, setCheckAut])
+   const loginSuccess = useSelector((state) => state.login)
 
+   useEffect(() => {
+      console.log('loginSuccess', loginSuccess);
+   }, [loginSuccess]);
 
-   // if (checkAut) {
-   //    return (
-   //       <h1 style={{ "color": "white" }}>Espere...</h1>
-   //    )
-   // }
+   useEffect(() => {
+      const auth = getAuth();
+      onAuthStateChanged(auth, (user) => {
+         if (user?.uid) {
+            setIsLogget(true)
+         } else {
+            setIsLogget(false)
+         }
+         setCheckAut(false)
+      })
+   }, [setIsLogget, setCheckAut])
+
+   if (checkAuth) {
+      return <LoadingScreen />
+   }
 
    return (
       <BrowserRouter>
          <Nav1 />
          <Routes>
+            <Route
+               path='/lg/home'
+               element={
+                  <PublicRoutes isAuth={isLogget}>
+                     <Home />
+                  </PublicRoutes>
+               }
+            />
 
+            <Route
+               path='/lg/mercado'
+               element={
+                  <PublicRoutes isAuth={isLogget}>
+                     <Mercado />
+                  </PublicRoutes>
+               }
+            />
 
-            <Route path='/home' element={
-               <PublicRoutes isAuth={isLogget} >
-                  <Home />
-               </PublicRoutes>
-            } />
-            <Route path='/mercado' element={
-               <PublicRoutes isAuth={isLogget} >
-                  <Mercado />
-               </PublicRoutes>
-            } />
-            <Route path='/aliados' element={
-               <PublicRoutes isAuth={isLogget} >
-                  <Aliados />
-               </PublicRoutes>
-            } />
-            <Route path='/agroinsumos' element={
-               <PublicRoutes isAuth={isLogget} >
-                  <Agroinsumos />
-               </PublicRoutes>
-            } />
-            <Route path='/nosotros' element={
-               <PublicRoutes isAuth={isLogget} >
-                  <Us />
-               </PublicRoutes>
-            } />
-            <Route path='/login' element={
-               <PublicRoutes isAuth={isLogget}>
-                  <LogIn />
-               </PublicRoutes>
-            } />
+            <Route
+               path='/lg/aliados'
+               element={
+                  <PublicRoutes isAuth={isLogget}>
+                     <Aliados />
+                  </PublicRoutes>
+               }
+            />
 
-            <Route path='/register' element={
-               <PublicRoutes isAuth={isLogget}>
-                  <Register />
-               </PublicRoutes>
-            } />
+            <Route
+               path='/lg/agroinsumos'
+               element={
+                  <PublicRoutes isAuth={isLogget}>
+                     <Agroinsumos />
+                  </PublicRoutes>
+               }
+            />
 
-            <Route path='/sobre-nosotros' element={
-               <PublicRoutes isAuth={isLogget}>
-                  <QuienesSomos />
-               </PublicRoutes>
-            } />
+            <Route
+               path='/lg/nosotros'
+               element={
+                  <PublicRoutes isAuth={isLogget}>
+                     <Us />
+                  </PublicRoutes>
+               }
+            />
 
-            <Route path='/*' element={
-               <PrivateRoutes isAuth={isLogget}>
-                  <DashboardRoutes />
-               </PrivateRoutes>
-            } />
+            <Route
+               path='/login'
+               element={
+                  <PublicRoutes isAuth={isLogget}>
+                     <LogIn />
+                  </PublicRoutes>
+               }
+            />
 
+            <Route
+               path='/register'
+               element={
+                  <PublicRoutes isAuth={isLogget}>
+                     <Register />
+                  </PublicRoutes>
+               }
+            />
+
+            <Route
+               path='/lg/sobre-nosotros'
+               element={
+                  <PublicRoutes isAuth={isLogget}>
+                     <QuienesSomos />
+                  </PublicRoutes>
+               }
+            />
+
+            <Route
+               path='/lg/user-dashboard'
+               element={
+                  <PublicRoutes isAuth={isLogget}>
+                     <User />
+                  </PublicRoutes>
+               }
+            />
+
+            <Route
+               path='/*'
+               element={
+                  <PrivateRoutes isAuth={isLogget}>
+                     <DashboardRoutes />
+                  </PrivateRoutes>
+               }
+            />
          </Routes>
       </BrowserRouter>
-   )
-}
+   );
+};
 
-export default AppRoutes
+export default AppRoutes;
--- src/routes/DashboardRoutes.jsx
+++ src/routes/DashboardRoutes.jsx
@@ -3,6 +3,8 @@ import Logeado from '../containers/Logeado'
 import { Routes, Route } from "react-router-dom";
 import AddProducto from '../containers/AddProducto';
 import Home from '../containers/Home';
+import User from '../containers/User';
+import QuienesSomos from '../containers/QuienesSomos';
 
 const DashboardRoutes = () => {
    return (
@@ -10,6 +12,10 @@ const DashboardRoutes = () => {
          <Routes>
             {/* <Route path='/logeado' element={<Logeado />} /> */}
             <Route path='/add' element={<AddProducto />} />
+            <Route path='/home' element={<Home />} />
+            <Route path='/user-dashboard' element={<User />} />
+            <Route path='/sobre-nosotros' element={<QuienesSomos />} />
+            
          </Routes>
       </>
    )
