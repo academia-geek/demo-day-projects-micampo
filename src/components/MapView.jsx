c7b2b680320630660b159b2fbbbffe9240a846b4
Santiago <santiagovalencialeon@gmail.com>
2022-07-25 02:31:16 -0500

    feat: posicion actual con marcadir agregada al mapa

--- src/components/MapView.jsx
+++ src/components/MapView.jsx
@@ -1,14 +1,48 @@
-import React from 'react';
-import { MapContainer, TileLayer } from 'react-leaflet';
+import React, { useEffect, useState } from 'react';
+import { MapContainer, TileLayer, Popup, Marker } from 'react-leaflet';
+import LoadingScreen from './LoadingScreen';
 
 const MapView = () => {
+   const [lat, setLat] = useState(0);
+   const [lng, setLng] = useState(0);
+   const getActualPosition = () => {
+      navigator.geolocation.getCurrentPosition((position) => {
+         const { latitude, longitude } = position.coords;
+         setLat(latitude);
+         setLng(longitude);
+      });
+   };
+
+   useEffect(() => {
+      getActualPosition();
+   }, []);
    return (
-      <MapContainer center={[51.505, -0.09]} zoom={13} scrollWheelZoom={false} style={{width: '80vw', height: '80vh'}}>
-         <TileLayer
-            attribution='&copy; <a href="https://www.openstreetmap.org/copyright">OpenStreetMap</a> contributors'
-            url='https://{s}.tile.openstreetmap.org/{z}/{x}/{y}.png'
-         />
-      </MapContainer>
+      <div
+         style={{
+            width: '100vw',
+            height: '80vh',
+            display: 'flex',
+            justifyContent: 'center',
+            alignItems: 'center',
+         }}>
+         {lat === 0 ? (
+            <LoadingScreen />
+         ) : (
+            <MapContainer
+               center={[lat, lng]}
+               zoom={13}
+               scrollWheelZoom={false}
+               style={{ width: '50vw', height: '50vh' }}>
+               <TileLayer
+                  attribution='&copy; <a href="https://www.openstreetmap.org/copyright">OpenStreetMap</a> contributors'
+                  url='https://{s}.tile.openstreetmap.org/{z}/{x}/{y}.png'
+               />
+               <Marker position={[lat, lng]}>
+                  <Popup>Tu ubicacion</Popup>
+               </Marker>
+            </MapContainer>
+         )}
+      </div>
    );
 };
 
