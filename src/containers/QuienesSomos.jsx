e4cf86e0f828f0b056d6ae9f4e6f1c969285b97a
Danna Bastidas <dannabas2001@gmail.com>
2022-07-25 15:52:07 -0500

    Tarjetas

--- src/containers/QuienesSomos.jsx
+++ src/containers/QuienesSomos.jsx
@@ -1,8 +1,9 @@
 import React from 'react'
+import { Back } from '../Styles/Nosotros'
 
 const QuienesSomos = () => {
    return (
-      <div>QuienesSomos</div>
+      <><Back><img src="https://res.cloudinary.com/dcsn54xoj/image/upload/v1658714169/MiCampo/Dise%C3%B1o_sin_t%C3%ADtulo_1_imf7pk.png" alt="" /></Back></>
    )
 }
 
