c52038cf7e4a5687b50d07c4449adb51a1c1a3f7
Santiago <santiagovalencialeon@gmail.com>
2022-07-25 05:18:24 -0500

    feat:  captura de datos al mover marcador de mapa

--- src/components/MapView.jsx
+++ src/components/MapView.jsx
@@ -6,6 +6,8 @@ const MapView = () => {
    const [actualPositionOrMove, setActualPositionOrMove] = useState(true);
    const [lat, setLat] = useState(0);
    const [lng, setLng] = useState(0);
+   const [position, setPosition] = useState(null);
+   const markerRef = useRef(null);
    const getActualPosition = () => {
       navigator.geolocation.getCurrentPosition((position) => {
          const { latitude, longitude } = position.coords;
@@ -14,9 +16,20 @@ const MapView = () => {
       });
    };
 
+   const dragend = () => {
+      const marker = markerRef.current;
+      if (marker != null) {
+         setPosition(marker.getLatLng());
+      }
+   };
+
    useEffect(() => {
       getActualPosition();
    }, []);
+
+   useEffect(() => {
+      console.log(position);
+   }, [position]);
 
    return (
       <div
@@ -28,15 +41,15 @@ const MapView = () => {
             alignItems: 'center',
             flexDirection: 'column',
          }}>
+         <h1>¿Que quieres hacer?</h1>
          {actualPositionOrMove ? (
             <>
-               <h1>¿Quieres guardar tu ubicacion actual?</h1>
-                <button onClick={() => setActualPositionOrMove(false)}>
-                    Cambiar ubicacion
-                </button>
-                <button onClick={() => setActualPositionOrMove(false)}>
-                    Guardar esta ubicacion
-                </button>
+               <button onClick={() => console.log(`lat: ${lat} - lng: ${lng}`)}>
+                  Guardar mi ubicación actual
+               </button>
+               <button onClick={() => setActualPositionOrMove(false)}>
+                  Cambiar a otra ubicación
+               </button>
                {lat !== 0 && lng !== 0 ? (
                   <MapContainer
                      center={[lat, lng]}
@@ -61,45 +74,50 @@ const MapView = () => {
             </>
          ) : (
             <>
-                <h1>¿Quieres guardar esta ubicacion <br /> como tu ubicacion por defecto?</h1>
-                <button onClick={() => setActualPositionOrMove(true)}>
-                    Cambiar a ubicacion actual
-                </button>
-                <button onClick={() => setActualPositionOrMove(false)}>
-                    Guardar esta ubicacion
-                </button>
-                <MapContainer
-                   center={[lat, lng]}
-                   zoom={17}
-                   scrollWheelZoom={true}
-                   style={{ width: '50vw', height: '50vh' }}>
-                   <TileLayer
-                      attribution='&copy; <a href="https://www.openstreetmap.org/copyright">OpenStreetMap</a> contributors'
-                      url='https://{s}.tile.openstreetmap.org/{z}/{x}/{y}.png'
-                   />
-                   <Marker
-                      position={[lat, lng]}
-                      draggable={true}
-                      //   onDragend={eventHandlers.dragend}
-                      //   ref={markerRef}
-                   >
-                      <Popup position={[lat, lng]}>
-                         <span>
-                            <strong style={{
+               <button onClick={() => setActualPositionOrMove(true)}>
+                  Cambiar a mi ubicación actual
+               </button>
+               <button onClick={() => console.log(`lat: ${lat} - lng: ${lng}`)}>
+                  Guardar esta ubicacion
+               </button>
+
+               <strong>Arrastra el marcador para cambiar la ubicación</strong>
+               <MapContainer
+                  center={[lat, lng]}
+                  zoom={17}
+                  scrollWheelZoom={true}
+                  style={{ width: '50vw', height: '50vh' }}>
+                  <TileLayer
+                     attribution='&copy; <a href="https://www.openstreetmap.org/copyright">OpenStreetMap</a> contributors'
+                     url='https://{s}.tile.openstreetmap.org/{z}/{x}/{y}.png'
+                  />
+                  <Marker
+                     position={position}
+                     draggable={true}
+                     eventHandlers={{
+                        dragend: dragend,
+                     }}
+                     dragend={dragend}
+                     ref={markerRef}>
+                     <Popup position={[lat, lng]}>
+                        <span>
+                           <strong
+                              style={{
                                  fontSize: '0.8rem',
-                                    fontWeight: 'bold',
-                                    color: '#000',
-                                    textAlign: 'center',
-                                    textTransform: 'uppercase',
-                                    letterSpacing: '1px',
-                                    marginBottom: '1rem',
-                                    marginTop: '1rem',
-                                    
-                            }}>Arrastrame</strong>
-                         </span>
-                      </Popup>
-                   </Marker>
-                </MapContainer>
+                                 fontWeight: 'bold',
+                                 color: '#000',
+                                 textAlign: 'center',
+                                 textTransform: 'uppercase',
+                                 letterSpacing: '1px',
+                                 marginBottom: '1rem',
+                                 marginTop: '1rem',
+                              }}>
+                              Arrastrame
+                           </strong>
+                        </span>
+                     </Popup>
+                  </Marker>
+               </MapContainer>
             </>
          )}
       </div>
