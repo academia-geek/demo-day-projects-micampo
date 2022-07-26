4f2678259ee46859c3358ad876b5f94fcc2d2c07
Santiago <santiagovalencialeon@gmail.com>
2022-07-26 00:55:43 -0500

    fix: chat global arreglado

--- src/components/chat/GlobalChat.jsx
+++ src/components/chat/GlobalChat.jsx
@@ -1,13 +1,42 @@
 import { getAuth } from 'firebase/auth';
-import { collection, getDocs } from 'firebase/firestore';
+import {
+   addDoc,
+   collection,
+   getDocs,
+   serverTimestamp,
+} from 'firebase/firestore';
 import React, { useEffect, useRef, useState } from 'react';
+import { useSelector } from 'react-redux';
 import { db } from '../../firebase/firebaseConfig';
 import SendMessages from './SendMessages';
 
 const GlobalChat = () => {
    const scroll = useRef();
    const [messages, setMessages] = useState([]);
+   const [msg, setMsg] = useState('');
    const auth = getAuth();
+   const user = useSelector((state) => state.user);
+
+   const sendMessage = async (e) => {
+      e.preventDefault();
+      const docRef = await getDocs(collection(db, 'messages'));
+      const id = docRef.docs.length + 1;
+
+      const photoURL = user.photoURL
+         ? user.photoURL
+         : 'https://res.cloudinary.com/gartners/image/upload/v1658122846/DemoDay/146-1468843_profile-icon-orange-png-transparent-png_qsx0un.png';
+
+      await addDoc(collection(db, 'messages'), {
+         id,
+         text: msg,
+         photoURL,
+         uid: user.uid,
+         name: user.name,
+         createdAt: serverTimestamp(),
+      });
+      setMsg('');
+      scroll.current.scrollIntoView({ behavior: 'smooth' });
+   };
 
    const collectionData = async () => {
       const consult = await getDocs(collection(db, 'messages'));
@@ -20,11 +49,10 @@ const GlobalChat = () => {
 
    useEffect(() => {
       collectionData();
-   }, []);
+   }, [sendMessage]);
 
    return (
       <div>
-         <div></div>
          <div className='msgs'>
             {messages.map(({ id, text, photoURL, uid, createdAt, name }) => (
                <div key={id}>
@@ -54,7 +82,30 @@ const GlobalChat = () => {
                </div>
             ))}
          </div>
-         <SendMessages scroll={scroll} />
+
+         <div>
+            <form onSubmit={sendMessage}>
+               <div className='sendMsg'>
+                  <input
+                     placeholder='Mensaje'
+                     type='text'
+                     value={msg}
+                     onChange={(e) => setMsg(e.target.value)}
+                  />
+                  <button
+                     style={{
+                        width: '18%',
+                        fontSize: '15px',
+                        fontWeight: '550',
+                        margin: '4px 5% -13px 5%',
+                        maxWidth: '200px',
+                     }}
+                     type='submit'>
+                     Enviar
+                  </button>
+               </div>
+            </form>
+         </div>
          <div ref={scroll}></div>
       </div>
    );
--- src/components/chat/SendMessages.jsx
+++ src/components/chat/SendMessages.jsx
@@ -9,29 +9,7 @@ import { useSelector } from 'react-redux';
 import { db } from '../../firebase/firebaseConfig';
 
 const SendMessages = ({ scroll }) => {
-   const [msg, setMsg] = useState('');
-   const user = useSelector((state) => state.user);
-
-   const sendMessage = async (e) => {
-      e.preventDefault();
-      const docRef = await getDocs(collection(db, 'messages'));
-      const id = docRef.docs.length + 1;
-
-      const photoURL = user.photoURL
-         ? user.photoURL
-         : 'https://res.cloudinary.com/gartners/image/upload/v1658122846/DemoDay/146-1468843_profile-icon-orange-png-transparent-png_qsx0un.png';
-
-      await addDoc(collection(db, 'messages'), {
-         id,
-         text: msg,
-         photoURL,
-         uid: user.uid,
-         name: user.name,
-         createdAt: serverTimestamp(),
-      });
-      setMsg('');
-      scroll.current.scrollIntoView({ behavior: 'smooth' });
-   };
+   
 
    return (
       <div>
